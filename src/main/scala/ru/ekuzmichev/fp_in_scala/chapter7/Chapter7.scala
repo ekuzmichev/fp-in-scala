e3068894b67f76f7b47ca2ffc950eb95440679e1
Evgenii Kuzmichev <evgenii.e.kuzmichev@gmail.com>
2019-03-03 22:53:38 +0300

    Chanter 7 Draft Par interface

--- src/main/scala/ru/ekuzmichev/fp_in_scala/chapter7/Chapter7.scala
+++ src/main/scala/ru/ekuzmichev/fp_in_scala/chapter7/Chapter7.scala
@@ -1,16 +1,26 @@
 package ru.ekuzmichev.fp_in_scala.chapter7
 
 object Chapter7 extends App {
-  def sum(ints: IndexedSeq[Int]): Int = {
+  def sum(ints: IndexedSeq[Int]): Par[Int] = {
     if (ints.size <= 1)
-      ints.headOption.getOrElse(0)
+      Par.unit(ints.headOption.getOrElse(0))
     else {
       val (l, r) = ints.splitAt(ints.size / 2)
-      sum(l) + sum(r)
+      Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
     }
   }
+}
 
-  val res = sum(Vector(1, 2, 3, 4, 5))
+trait Par[A]
 
-  println(s"Result: $res")
+object Par {
+  def unit[A](a: A): Par[A] = ???
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def run[A](a: Par[A]): A = ???
+
+  def map2[A, B, C](parA: Par[A], parB: Par[B])(f: (A, B) => C): Par[C] = ???
+
+  def fork[A](a: => Par[A]): Par[A] = ???
 }
